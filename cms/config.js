5b9b24ed6daddadeffa9cba79f5bda1a7c7d3e75
ujjwalsingh.11 <ujjwalsingh7985@gmail.com>
2026-06-29 21:50:35 +0530

    Add Supabase configuration to config.js

---
+++ cms/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://mensuwvekohlwwaacilf.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_LjdB8fJNoVqMnP_GQL5TMA_Se1tcmtr";
+const cms = supabase.createClient(sb_publishable_LjdB8fJNoVqMnP_GQL5TMA_Se1tcmtr);
