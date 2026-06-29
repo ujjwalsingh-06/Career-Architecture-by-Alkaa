9a508b77d3b01fd2cfbba0cc3f14fc41ee3553ec
ujjwalsingh.11 <ujjwalsingh7985@gmail.com>
2026-06-29 14:32:39 +0530

    Add Supabase configuration file

---
+++ js/supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://mensuwvekohlwwaacilf.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_LjdB8fJNoVqMnP_GQL5TMA_Se1tcmtr";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
